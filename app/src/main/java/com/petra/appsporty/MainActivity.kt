dfd2cd55416ab3da517e79049f0eb30a8146c9db
c14210254 <120379250+c14210254@users.noreply.github.com>
2023-12-09 11:31:51 +0700

    hai bang

--- app/src/main/java/com/petra/appsporty/MainActivity.kt
+++ app/src/main/java/com/petra/appsporty/MainActivity.kt
@@ -2,11 +2,40 @@ package com.petra.appsporty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.petra.appsporty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragmentManager : FragmentManager
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navBarHome.setOnClickListener {
+            goToFragment(fragment_home())
+        }
+
+        binding.imgProfile.setOnClickListener {
+            goToFragment(fragment_profile())
+        }
+
+        binding.navBarList.setOnClickListener {
+            //nanti panggil halaman list nya d sni
+        }
+
+        binding.navBarFav.setOnClickListener {
+            //nanti panggil halaman favorit nya di sni
+        }
+
+    }
+
+    private fun goToFragment(fragment: Fragment) {
+        fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.fragmentContainerView, fragment).commit()
     }
 }
